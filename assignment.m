bd0887211aac729f251f4a9387f0375e2f5adf46
ismail_gokbas <55627469+ismail119@users.noreply.github.com>
2022-05-25 01:19:54 +0300

    Update assignment.m

--- assignment.m
+++ assignment.m
@@ -1,5 +1,4 @@
-
-% Ismail Gokbas, Demircan Demir,Ismet Mert EskiBorekcioglu tarafindan hazirlanmistir.
+% Ismail Gokbas MATLAB-FUZZY
 
 fis = mamfis('Name',"MyFis");
 fis = addInput(fis,[7 16],"Name","Temperature");
@@ -11,8 +10,6 @@ fis = addMF(fis,"Temperature","trimf",[12 14 16],'Name',"H");
 fis = addMF(fis,"Temperature","trimf",[13 16 16],'Name',"VH");
 
 
-% Ismail Gokbas, Demircan Demir,Ismet Mert EskiBorekcioglu tarafindan hazirlanmistir.
-
 fis = addInput(fis,[1.75 4],"Name","Pressure");
 fis = addMF(fis,"Pressure","trimf",[1.75 1.75 2.25],'Name',"VB");
 fis = addMF(fis,"Pressure","trimf",[1.75 2.25 2.50],'Name',"B");
@@ -20,7 +17,7 @@ fis = addMF(fis,"Pressure","trimf",[2.25 2.75 3.25],'Name',"N");
 fis = addMF(fis,"Pressure","trimf",[2.50 3.25 3.50],'Name',"G");
 fis = addMF(fis,"Pressure","trimf",[2.75 4 4],'Name',"VG");
 
-% Ismail Gokbas, Demircan Demir,Ismet Mert EskiBorekcioglu tarafindan hazirlanmistir.
+
 
 fis = addOutput(fis,[2 6],"Name","PercentageOfCarbonDioxide");
 fis = addMF(fis,"PercentageOfCarbonDioxide","trimf",[2 2 3],'Name',"VB");
@@ -29,7 +26,6 @@ fis = addMF(fis,"PercentageOfCarbonDioxide","trimf",[3 4 5],'Name',"N");
 fis = addMF(fis,"PercentageOfCarbonDioxide","trimf",[4 5 6],'Name',"G");
 fis = addMF(fis,"PercentageOfCarbonDioxide","trimf",[5 6 6],'Name',"VG");
 
-% Ismail Gokbas, Demircan Demir,Ismet Mert EskiBorekcioglu tarafindan hazirlanmistir.
 
 
 rule1 = [1,1,3,1,1]; rule2 = [1,2,3,1,1]; rule3 = [1,3,4,1,1]; rule4 = [1,4,5,1,1]; rule5 = [1,5,5,1,1];
@@ -42,12 +38,10 @@ rule16 = [4,1,2,1,1]; rule17 = [4,2,2,1,1]; rule18 = [4,3,3,1,1]; rule19 = [4,4,
 
 rule21 = [4,1,1,1,1]; rule22 = [4,2,2,1,1]; rule23 = [4,3,3,1,1]; rule24 = [4,4,3,1,1]; rule25 = [4,5,4,1,1];
 
-% Ismail Gokbas, Demircan Demir,Ismet Mert EskiBorekcioglu tarafindan hazirlanmistir.
 
 ruleList = [rule1;rule2;rule3;rule4;rule5;rule6;rule7;rule8;rule9;rule10;rule11;rule12;rule13;rule14;
     rule15;rule16;rule17;rule18;rule19;rule20;rule21;rule22;rule23;rule24;rule25];
 
-%Ismail Gokbas
 fis2 = addRule(fis,ruleList);
 
 
